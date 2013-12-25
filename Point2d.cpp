5d3cf60b428538e7d0de40634aae86984dafc0b0
CleanSoup <cleansoup@gmail.com>
2013-12-25 17:32:49 -0500

    Added broken 2 dimensional point class

---
+++ Point2d.cpp
@@ -0,0 +1,14 @@
+#include "Point2d.h"
+Point2d::Point2d(int x, int y) {
+	setPos(x, y);
+}
+int Point2d::getX() {
+	return x;
+}
+int Point2d::getY() {
+    return y;
+}
+void Point2d::setPos(int x, int y) {
+    this->x = x;
+    this->y = y;
+}
---
+++ Point2d.h
@@ -0,0 +1,14 @@
+#ifndef POINT2D_H_INCLUDED
+#define POINT2D_H_INCLUDED
+class Point2d {
+	private:
+		int x;
+		int y;
+	public:
+		Point2d(int x, int y);
+		int getX();
+		int getY();
+		void setPos(int x, int y);
+};
+
+#endif // POINT2D_H_INCLUDED
