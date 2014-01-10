423ee5c5a76db3c895fc1453689feb5aae1c7288
Carden Bagwell <cardenbag@gmail.com>
2014-01-10 14:04:44 -0800

    Moved the include statements from main.cpp to main.h.

--- include/main.h
+++ include/main.h
@@ -1,3 +1,8 @@
+#include "Force2d.h"
+#include "Point2d.h"
+#include "Velocity2d.h"
+#include "Puck.h"
+
 void game_loop(SDL_Window* window);
 void game_init(SDL_Surface* screenSurface);
 void clear(SDL_Surface* screenSurface);
