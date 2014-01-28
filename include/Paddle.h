a1f5540a86033dc6999c72c792bfb833c9d5bcfe
Carden Bagwell <cardenbag@gmail.com>
2014-01-28 11:35:59 -0800

    Implemented prototype constructors for the Paddle class to experiment with drawing paddles on the screen. Commented out anything associated with the Point2d class. Not ready to utilize that functionality yet.

--- include/Paddle.h
+++ include/Paddle.h
@@ -1,13 +1,15 @@
 #ifndef PADDLE_H_INCLUDED
 #define PADDLE_H_INCLUDED
+#include "Global.h"
+
 class Paddle {
 	private:
-		Point2d myPosition;
+		//Point2d myPosition;
 	public:
 		Paddle();
 		Paddle(int xPos, int yPos);
-		Point2d getPosition();
-		void movePosition(MovementVector2d aDirection);
+		//Point2d getPosition();
+		//void movePosition(MovementVector2d aDirection);
 
-}
+};
 #endif //PADDLE_H_INCLUDED
--- src/Paddle.cpp
+++ src/Paddle.cpp
@@ -1,7 +1,13 @@
+#include "Paddle.h"
+
+
 Paddle::Paddle() {
+	SDL_Rect rectToDraw = {0, 0, 70, 70};
+	SDL_RenderDrawRect(Global::getRenderer(), &rectToDraw);
 
 }
 Paddle::Paddle(int xPos, int yPos) {
+	Paddle();
 
 }
 
